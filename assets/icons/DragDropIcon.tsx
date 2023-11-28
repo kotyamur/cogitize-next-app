739208b356831a7a079ad7f6f55fde57edfe4b8e
Kateryna Kleshch <ketrins1993@gmail.com>
2023-11-28 21:39:57 +0200

    add changes

--- assets/icons/DragDropIcon.tsx
+++ assets/icons/DragDropIcon.tsx
@@ -7,12 +7,12 @@ const GrapIcon = () => {
       viewBox="0 0 12 20"
       fill="none"
     >
-      <circle cx="2" cy="2" r="2" fill="#F5F5F5" fill-opacity="0.08" />
-      <circle cx="2" cy="10" r="2" fill="#F5F5F5" fill-opacity="0.08" />
-      <circle cx="2" cy="18" r="2" fill="#F5F5F5" fill-opacity="0.08" />
-      <circle cx="10" cy="2" r="2" fill="#F5F5F5" fill-opacity="0.08" />
-      <circle cx="10" cy="10" r="2" fill="#F5F5F5" fill-opacity="0.08" />
-      <circle cx="10" cy="18" r="2" fill="#F5F5F5" fill-opacity="0.08" />
+      <circle cx="2" cy="2" r="2" fill="#F5F5F5" fillOpacity="0.08" />
+      <circle cx="2" cy="10" r="2" fill="#F5F5F5" fillOpacity="0.08" />
+      <circle cx="2" cy="18" r="2" fill="#F5F5F5" fillOpacity="0.08" />
+      <circle cx="10" cy="2" r="2" fill="#F5F5F5" fillOpacity="0.08" />
+      <circle cx="10" cy="10" r="2" fill="#F5F5F5" fillOpacity="0.08" />
+      <circle cx="10" cy="18" r="2" fill="#F5F5F5" fillOpacity="0.08" />
     </svg>
   );
 };
--- components/DutiesForm/DutiesForm.tsx
+++ components/DutiesForm/DutiesForm.tsx
@@ -2,12 +2,58 @@
 import { FC } from "react";
 import styles from "./DutiesForm.module.scss";
 
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
 import CheckIcon from "@/assets/icons/CheckIcon";
+
+const responsibilities = [
+  {
+    name: "showdown",
+    valueList: ["duel", "make-claims"],
+    text: "Разборки",
+    labelsText: ["Дуель", "Выставлять претензии"],
+  },
+];
 
 type Inputs = {
   name: string;
   trading: string | string[];
+  showdown: string | string[];
+};
+
+type Props = {
+  register: UseFormRegister<Inputs>
+};
+
+export const LabelGroup = ({register}: Props) => {
+  return (
+    <div className={styles.responsibilityGroup}>
+      <p className={styles.checkboxHeading}>Разборки</p>
+      <label>
+        <input
+          type="checkbox"
+          {...register("showdown")}
+          value="duel"
+          className={`${styles.checkboxIcon} visuallyHidden`}
+        />
+        <span className={styles.customIcon}>
+          <CheckIcon />
+        </span>
+        <span className={styles.responsibilityItem}>Дуель</span>
+      </label>
+      <label>
+        <input
+          type="checkbox"
+          {...register("showdown")}
+          value="make-claims"
+          className={`${styles.checkboxIcon} visuallyHidden`}
+        />
+        <span className={styles.customIcon}>
+          <CheckIcon />
+        </span>
+        <span className={styles.responsibilityItem}>Выставлять претензии</span>
+      </label>
+    </div>
+  );
 };
 
 export const DutiesForm: FC = () => {
@@ -28,45 +74,50 @@ export const DutiesForm: FC = () => {
           <div className={styles.boxHeading}>
             <p>Обязаности</p>
           </div>
-          <p className={styles.checkboxHeading}>Торговля</p>
-          <label>
-            <input
-              type="checkbox"
-              {...register("trading")}
-              value="sale-product"
-              className={`${styles.checkboxIcon} visuallyHidden`}
-            />
-            <span className={styles.customIcon}>
-              <CheckIcon />
-            </span>
-            <span className={styles.responsibilitiItem}>Продавать продукт</span>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              {...register("trading")}
-              value="set-prices"
-              className={`${styles.checkboxIcon} visuallyHidden`}
-            />
-            <span className={styles.customIcon}>
-              <CheckIcon />
-            </span>
-            <span className={styles.responsibilitiItem}>Выставлять цены</span>
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              {...register("trading")}
-              value="view-analytics"
-              className={`${styles.checkboxIcon} visuallyHidden`}
-            />
-            <span className={styles.customIcon}>
-              <CheckIcon />
-            </span>
-            <span className={styles.responsibilitiItem}>
-              Смотреть аналитику
-            </span>
-          </label>
+          <div className={styles.responsibilityGroup}>
+            <p className={styles.checkboxHeading}>Торговля</p>
+            <label>
+              <input
+                type="checkbox"
+                {...register("trading")}
+                value="sale-product"
+                className={`${styles.checkboxIcon} visuallyHidden`}
+              />
+              <span className={styles.customIcon}>
+                <CheckIcon />
+              </span>
+              <span className={styles.responsibilityItem}>
+                Продавать продукт
+              </span>
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                {...register("trading")}
+                value="set-prices"
+                className={`${styles.checkboxIcon} visuallyHidden`}
+              />
+              <span className={styles.customIcon}>
+                <CheckIcon />
+              </span>
+              <span className={styles.responsibilityItem}>Выставлять цены</span>
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                {...register("trading")}
+                value="view-analytics"
+                className={`${styles.checkboxIcon} visuallyHidden`}
+              />
+              <span className={styles.customIcon}>
+                <CheckIcon />
+              </span>
+              <span className={styles.responsibilityItem}>
+                Смотреть аналитику
+              </span>
+            </label>
+          </div>
+          <LabelGroup register={register} />
         </div>
 
         <input className={styles.submitBtn} type="submit" value="Сохранить" />
