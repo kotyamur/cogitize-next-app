3379a8408f013754a897dff8ff652071c14ffc08
Kateryna Kleshch <ketrins1993@gmail.com>
2023-12-18 21:06:49 +0200

    made DragDrop without libraries

--- components/PositionItem/PositionItem.tsx
+++ components/PositionItem/PositionItem.tsx
@@ -1,34 +1,81 @@
+import { DragEvent, SetStateAction, Dispatch } from "react";
+import DragDropIcon from "../../assets/icons/DragDropIcon";
+import { listItem } from "../PositionsList/PositionsList";
 import styles from "./PositionsItem.module.scss";
-import DragDropIcon from "../../assets/icons/DragDropIcon"
 
 type Props = {
-    key: string;
-    item: {
-        id: string;
-        name: string;
-        payment: number;
-        tasks: number;
-    };
-}
+  key: string;
+  item: listItem;
+  setCurrentCard: Dispatch<SetStateAction<listItem | null>>;
+  cardList: listItem[];
+  setCardList: Dispatch<SetStateAction<listItem[]>>;
+  currentCard: listItem | null;
+};
 
-export const PositionsItem = ({ item }: Props) => {
-    const { name, payment, tasks } = item;
-    return (
-      <li className={styles.container}>
-        <button type="button" className={styles.dragButton}>
-          <DragDropIcon />
-        </button>
-        <div className={styles.positionInfo}>
-          <div>
-            <p className={styles.positionName}>{name}</p>
-            <p>{tasks} заданий</p>
-          </div>
-          <div>
-            <p className={styles.positionPayment}>
-              <span>${payment}</span> / час
-            </p>
-          </div>
+export const PositionsItem = ({
+  item,
+  setCurrentCard,
+  cardList,
+  setCardList,
+  currentCard,
+}: Props) => {
+  const { name, payment, tasks } = item;
+  const dragStartHendler = (e: DragEvent<HTMLElement>, item: listItem) => {
+    e.currentTarget.style.boxShadow = "4px 8px 40px 0px rgba(0, 0, 0, 0.40)";
+    setCurrentCard(item);
+    console.log(item)
+  };
+  const dragLeaveHendler = (e: DragEvent<HTMLElement>) => {
+    e.currentTarget.style.border = "none";
+  };
+  const dragOverHendler = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    e.currentTarget.style.border = "2px solid rgba(245, 245, 245, 0.08)";
+  };
+  const dragEndHendler = (e: DragEvent<HTMLElement>) => {
+    e.currentTarget.style.boxShadow = "";
+  };
+  const dropHendler = (e: DragEvent<HTMLElement>, item: listItem) => {
+    e.preventDefault();
+    if (currentCard !== null) {
+      setCardList(
+        cardList.map((c) => {
+          if (c.id === item.id) {
+            return { ...c, order: currentCard.order };
+          }
+          if (c.id === currentCard.id) {
+            return { ...c, order: item.order };
+          }
+          return c;
+        })
+      );
+    }
+    e.currentTarget.style.border = "none";
+  };
+  return (
+    <li
+      className={styles.container}
+      draggable={true}
+      onDragStart={(e: DragEvent<HTMLElement>) => dragStartHendler(e, item)}
+      onDragLeave={(e: DragEvent<HTMLElement>) => dragLeaveHendler(e)}
+      onDragOver={(e: DragEvent<HTMLElement>) => dragOverHendler(e)}
+      onDragEnd={(e: DragEvent<HTMLElement>) => dragEndHendler(e)}
+      onDrop={(e: DragEvent<HTMLElement>) => dropHendler(e, item)}
+    >
+      <button type="button" className={styles.dragButton}>
+        <DragDropIcon />
+      </button>
+      <div className={styles.positionInfo}>
+        <div>
+          <p className={styles.positionName}>{name}</p>
+          <p>{tasks} заданий</p>
         </div>
-      </li>
-    );
+        <div>
+          <p className={styles.positionPayment}>
+            <span>${payment}</span> / час
+          </p>
+        </div>
+      </div>
+    </li>
+  );
 };
--- components/PositionsList/PositionsList.tsx
+++ components/PositionsList/PositionsList.tsx
@@ -1,23 +1,50 @@
-import { FC } from "react";
+"use client";
+import { FC, useState } from "react";
 import styles from "./PositionsList.module.scss";
 import { PositionsItem } from "../PositionItem/PositionItem";
 
 const positionList = [
-  { id: "1", name: "Новобранец", payment: 50, tasks: 0 },
-  { id: "2", name: "Рядовой", payment: 80, tasks: 5 },
-  { id: "3", name: "Сержант", payment: 100, tasks: 10 },
-  { id: "4", name: "Новобранец", payment: 50, tasks: 10 },
-  { id: "5", name: "Рядовой", payment: 80, tasks: 15 },
-  { id: "6", name: "Сержант", payment: 100, tasks: 20 },
-  { id: "7", name: "Новобранец", payment: 50, tasks: 25 },
+  { id: "1", name: "Новобранец", payment: 50, tasks: 0, order: 1 },
+  { id: "2", name: "Рядовой", payment: 80, tasks: 5, order: 2 },
+  { id: "3", name: "Сержант", payment: 100, tasks: 10, order: 3 },
+  { id: "4", name: "Новобранец", payment: 50, tasks: 10, order: 4 },
+  { id: "5", name: "Рядовой", payment: 80, tasks: 15, order: 5 },
+  { id: "6", name: "Сержант", payment: 100, tasks: 20, order: 6 },
+  { id: "7", name: "Новобранец", payment: 50, tasks: 25, order: 7 },
 ];
 
+export type listItem = {
+  id: string;
+  name: string;
+  payment: number;
+  tasks: number;
+  order: number;
+};
+
 export const PositionsList: FC = () => {
+  const [cardList, setCardList] = useState(positionList);
+  const [currentCard, setCurrentCard] = useState<listItem | null>(null);
+  const sortCards = (a: listItem, b: listItem) => {
+    if (a.order > b.order) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
   return (
     <div className={styles.positionsContainer}>
       <ul className={styles.positionList}>
-        {positionList.map((item) => {
-          return <PositionsItem key={item.id} item={item} />;
+        {cardList.sort(sortCards).map((item) => {
+          return (
+            <PositionsItem
+              key={item.id}
+              item={item}
+              currentCard={currentCard}
+              setCurrentCard={setCurrentCard}
+              cardList={cardList}
+              setCardList={setCardList}
+            />
+          );
         })}
       </ul>
       <button type="button" className={styles.createBtn}>
